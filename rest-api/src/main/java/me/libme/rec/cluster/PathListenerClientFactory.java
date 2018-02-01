c942508ee33b43c9f06e429355a450ad9a6667e9
J <3cgg@163.com>
2018-02-01 14:08:25 +0800

    -

--- rest-api/src/main/java/me/libme/rec/cluster/PathListenerClientFactory.java
+++ rest-api/src/main/java/me/libme/rec/cluster/PathListenerClientFactory.java
@@ -7,6 +7,9 @@ import me.libme.module.zookeeper.ZooKeeperConnector;
 import scalalg.me.libme.rec.RecRuntime;
 
 import java.lang.reflect.Proxy;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by J on 2018/1/30.
@@ -14,17 +17,35 @@ import java.lang.reflect.Proxy;
 public class PathListenerClientFactory {
 
 
+
+    private static AtomicReference<ClientChannelExecutor> clientChannelExecutorHolder=new AtomicReference<>();
+
+    private static Lock lock=new ReentrantLock();
+
     public static  <T> T factory(Class<T> intarface,String path){
 
-        RecRuntime recRuntime=RecRuntime.builder().getOrCreate();
+        if(clientChannelExecutorHolder.get()==null){
+            try{
+                lock.lock();
 
-        //zookeeper
-        ZooKeeperConnector.ZookeeperExecutor executor=recRuntime.zookeeperExecutor().get();
+                if(clientChannelExecutorHolder.get()==null){
+                    RecRuntime recRuntime=RecRuntime.builder().getOrCreate();
 
-        ChannelInfoOnZkProvider channelInfoOnZkProvider=new ChannelInfoOnZkProvider(ClusterZkPaths.LEADER_INFO_PATH,executor);
-        ClientChannelExecutor clientChannelExecutor=new DynamicClientChannelExecutor(channelInfoOnZkProvider);
+                    //zookeeper
+                    ZooKeeperConnector.ZookeeperExecutor executor=recRuntime.zookeeperExecutor().get();
 
-        SimpleClient simpleClient=new SimpleClient(clientChannelExecutor,path);
+                    ChannelInfoOnZkProvider channelInfoOnZkProvider=new ChannelInfoOnZkProvider(ClusterZkPaths.LEADER_INFO_PATH,executor);
+                    ClientChannelExecutor clientChannelExecutor=new DynamicClientChannelExecutor(channelInfoOnZkProvider);
+                    clientChannelExecutorHolder.set(clientChannelExecutor);
+                }
+
+            }finally {
+                lock.unlock();
+            }
+
+        }
+
+        SimpleClient simpleClient=new SimpleClient(clientChannelExecutorHolder.get(),path);
         return (T) Proxy.newProxyInstance(intarface.getClassLoader(),new Class[]{intarface},simpleClient);
 
     }
