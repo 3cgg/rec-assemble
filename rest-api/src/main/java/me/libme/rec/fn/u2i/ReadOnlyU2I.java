edf6790a1c851ee8b7a3152b87822032d953079f
J <3cgg@163.com>
2018-02-02 10:14:22 +0800

    -

--- rest-api/src/main/java/me/libme/rec/fn/u2i/ReadOnlyU2I.java
+++ rest-api/src/main/java/me/libme/rec/fn/u2i/ReadOnlyU2I.java
@@ -4,6 +4,9 @@ import me.libme.kernel._c.cache.JCacheService;
 import me.libme.kernel._c.cache.JMapCacheService;
 import me.libme.rec.cluster.PathListenerClientFactory;
 import me.libme.rec.cluster._trait.Unique2IntMark;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import scalalg.me.libme.rec.RecRuntime;
 
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -13,12 +16,29 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class ReadOnlyU2I implements IUnique2Int{
 
+    private static final Logger LOGGER= LoggerFactory.getLogger(ReadOnlyU2I.class);
 
-    private static JCacheService<String,Integer> cacheService=new JMapCacheService<>();
+    private JCacheService<String,Integer> cacheService=new JMapCacheService<>();
 
-    private static final ReadOnlyU2I instance=new ReadOnlyU2I();
+    private static ReadOnlyU2I instance;
 
-    public static ReadOnlyU2I get(){return instance;}
+    public static ReadOnlyU2I get(){
+
+        if(instance==null){
+            synchronized (ReadOnlyU2I.class){
+                if(instance==null){
+                    RecRuntime recRuntime=RecRuntime.builder().getOrCreate();
+                    Unique2IntRepo unique2IntRepo=new HBaseRepo(recRuntime.u2iTable(),recRuntime.u2iFamily()
+                            ,recRuntime.u2iColumn(),recRuntime.hbaseExecutor().get());
+                    instance=new ReadOnlyU2I();
+                    int max=unique2IntRepo.initialize(instance.cacheService);
+                    LOGGER.info("worker collect all elements of u2i collection, the max value : "+max);
+                }
+            }
+        }
+
+        return instance;
+    }
 
     private Lock lock=new ReentrantLock();
 
